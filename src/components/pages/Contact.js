b3d6503e3f1025c0a57b8746c4eeda15b48ff54c
Shannon-Blade <shannoninengland@gmail.com>
2023-02-04 12:53:23 +0000

    added content to contact me page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,29 +1,29 @@
 import React from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
-import Learn from './Resume';
+import Resume from './Resume';
 
 function Contact(props) {
   return (
     <div>
-      <h1>Contact Page</h1>
+      <h1>Seek, and you will find:</h1>
       <p>
-      Checklist
+      I would love to hear from you! Whether you have a question, suggestion, or just want to say hello, please don't hesitate to reach out to me. You can contact me through the following methods:
       </p>
       <ul>
-        <li> Your name</li>
-        <li>[ ] GitHub Profile</li>
-        <li>[ ] LinkedIn Page</li>
-        <li>[ ] Email Address</li>
-        <li>[ ] Phone Number - (optional)</li>
+        <li>Shannon Yap</li>
+        <li><a href="https://github.com/damnsemicolon" target="_blank">Visit my GitHub Profile</a></li>
+
+        <li><a href="https://linkedin.com/in/shannonyap" target="blank">Visit my LinkedIn Page</a></li>
+        <li><a href="mailto:shannoninengland@gmail.com" target="_blank">Send me an Email</a></li>
+        <li><a href="tel:+44-7878-715906">Call me maybe at +44 (0) 7878-715906</a></li>
       </ul>
-      <Link to="learn" role="button" className="btn btn-link">
-        Learn More
+      
+      <Link to="resume" role="button" className="btn-primary">
+        Resume
       </Link>
-      <Link to="contact" role="button" className="btn btn-link">
-        Learn Less
-      </Link>
+    
       <Routes>
-        <Route path="learn" element={<Learn />} />
+        <Route path="resume" element={<Resume />} />
       </Routes>
     </div>
   );
